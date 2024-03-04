77cc226ca2833607fb2281398c0c547da0885f9e
NicolaTeora <nikxodia@gmail.com>
2024-03-04 19:53:02 +0100

    feat : start project

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+//create un file index.php in cui è definita una classe Production
+class Production {
+    
+    //All'interno della classe dovrete gestire un titolo, una lingua e un voto
+    //La classe deve avere le sue variabili d'istanza, il costruttore e i metodi.
+    # variabili o proprietà
+    public $title;
+    public $lenguage;
+    public $vote;
+
+    # metodi
+    function set_film($title, $lenguage, $vote) {
+        
+    }
+}
+
+?>
